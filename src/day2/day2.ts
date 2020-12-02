cbde9546b664780e85b82c177c69442d66058fd0
Galen Elias <gelias@gmail.com>
2020-12-01 21:36:40 -0800

    Day 2 - TypeScript

---
+++ src/day2/day2.ts
@@ -0,0 +1,37 @@
+
+import { getPuzzleInput } from '../util/input.js'
+
+export async function run() {
+	let inputStr = await getPuzzleInput();
+
+	const pwd_regex = /(\d+)-(\d+) (\w): (\w+)/;
+	let valid_passwords_1 = 0;
+	let valid_passwords_2 = 0;
+	for (var line of inputStr) {
+		const matches = line.match(pwd_regex)!;
+		const [range1, range2, pwd_letter, password] = matches.slice(1);
+
+		const pwd_letter_count = [...password].filter((ch) => ch == pwd_letter).length;
+		if (pwd_letter_count >= +range1 && pwd_letter_count <= +range2) {
+			valid_passwords_1++;
+		}
+
+		let pt2_matches = 0;
+		if (password[+range1 - 1] == pwd_letter) {
+			pt2_matches++;
+		}
+
+		if (password[+range2 - 1] == pwd_letter) {
+			pt2_matches++;
+		}
+
+		if (pt2_matches == 1) {
+			valid_passwords_2++;
+		}
+	}
+
+	console.log("Part 1: ", valid_passwords_1);
+	console.log("Part 2: ", valid_passwords_2);
+}
+
+run();
