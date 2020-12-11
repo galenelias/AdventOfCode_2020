b021893e9c6e29564a9c0b9d6ae4c74243d1da2d
Galen Elias <gelias@gmail.com>
2020-12-11 09:13:16 -0800

    Day 11 - TypeScript cleaned up

--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -1,49 +1,58 @@
 import { getPuzzleInput } from '../util/input.js'
 
-interface Coord {
-	r: number,
-	c: number,
+function is_outside(inputs: string[][], r: number, c: number): boolean {
+	return r < 0 || r >= inputs.length || c < 0 || c >= inputs[r].length;
 }
 
-interface Node {
-	pos: Coord,
-	dist: number,
-}
-
-function get_safe(inputs: string[][], r: number, c: number): string {
-	if (r < 0 || r >= inputs.length || c < 0 || c >= inputs[r].length) {
-		return '.';
+function get(inputs: string[][], r: number, c: number): string {
+	if (is_outside(inputs, r, c)) {
+		return 'L';
 	} else {
 		return inputs[r][c];
 	}
 }
 
-function count_adjacent(inputs: string[][], r: number, c: number): number {
-	return (get_safe(inputs, r-1, c-1) == '#' ? 1 : 0) +
-		(get_safe(inputs, r-1, c) == '#' ? 1 : 0) +
-		(get_safe(inputs, r-1, c+1) == '#' ? 1 : 0) +
-		(get_safe(inputs, r, c-1) == '#' ? 1 : 0) +
-		(get_safe(inputs, r, c+1) == '#' ? 1 : 0) +
-		(get_safe(inputs, r+1, c-1) == '#' ? 1 : 0) +
-		(get_safe(inputs, r+1, c) == '#' ? 1 : 0) +
-		(get_safe(inputs, r+1, c+1) == '#' ? 1 : 0);
+function count_seats(grid: string[][]): number {
+	let seats = 0;
+	for (let r = 0; r < grid.length; r++) {
+		for (let c = 0; c < grid[r].length; c++) {
+			if (grid[r][c] == '#') {
+				seats++;
+			}
+		}
+	}
+	return seats;
 }
 
-function count_adjacent2(inputs: string[][], orig_r: number, orig_c: number): number {
+function count_adjacent(inputs: string[][], r: number, c: number): number {
 	let count = 0;
 	for (let dr of [-1, 0, 1]) {
 		for (let dc of [-1, 0, 1]) {
-			let r = orig_r;
-			let c = orig_c;
+			if ((dr != 0 || dc != 0) && get(inputs, r + dr, c + dc) == '#') {
+				count += 1;
+			}
+		}
+	}
+	return count;
+}
+
+function count_adjacent2(inputs: string[][], in_r: number, in_c: number): number {
+	let count = 0;
+	for (let dr of [-1, 0, 1]) {
+		for (let dc of [-1, 0, 1]) {
+			let r = in_r;
+			let c = in_c;
 			if (dr !== 0 || dc !== 0) {
 				r += dr;
 				c += dc;
 
-				while (r >= 0 && r < inputs.length && c >= 0 && c < inputs[0].length && inputs[r][c] == '.') {
+				// Project along dr/dc vector until we hit a non-blank
+				while (!is_outside(inputs, r, c) && inputs[r][c] == '.') {
 					r += dr;
 					c += dc;
 				}
-				if (get_safe(inputs, r, c) == '#') {
+
+				if (get(inputs, r, c) == '#') {
 					count++;
 				}
 			}
@@ -53,102 +62,59 @@ function count_adjacent2(inputs: string[][], orig_r: number, orig_c: number): nu
 	return count;
 }
 
-function print_grid(grid: string[][]) {
-	for (const row of grid) {
-		console.log(row.join(""));
-	}
-	console.log();
-}
-
-export async function run() {
-	let inputs = (await getPuzzleInput()).map(i => i.split(''));
-	// let inputs_orig = JSON.stringify(inputs);
-	let inputs_orig = inputs.map(row => row.slice());//JSON.stringify(inputs);
-
-	inputs[1][1] = 'x';
-
+function part1(grid: string[][]) {
 	let steps = 0;
-	let last: string = "";
-	// while (true) {
-	// 	steps++;
-
-	// 	let json = JSON.stringify(inputs);
-	// 	if (json === last) {
-	// 		let seats = 0;
-	// 		for (let r = 0; r < inputs.length; r++) {
-	// 			for (let c = 0; c < inputs[r].length; c++) {
-	// 				if (inputs[r][c] == '#') {
-	// 					seats++;
-	// 				}
-	// 			}
-	// 		}
-	// 		console.log(`Stabilized after ${steps} steps.  Seats = ${seats}`);
-
-	// 		break;
-	// 	} else {
-	// 		last = json;
-	// 	}
-
-	// 	let newGrid = JSON.parse(json);
-
-	// 	let x = count_adjacent(inputs, 0, 2);
-
-	// 	for (let r = 0; r < inputs.length; r++) {
-	// 		for (let c = 0; c < inputs[r].length; c++) {
-	// 			const ch = inputs[r][c];
-
-	// 			if (ch == 'L' && count_adjacent(inputs, r, c) == 0) {
-	// 				newGrid[r][c] = "#";
-	// 			} else if (ch == '#' && count_adjacent(inputs, r, c) >= 4) {
-	// 				newGrid[r][c] = 'L';
-	// 			}
-	// 		}
-	// 	}
-	// 	inputs = newGrid;
-
-	// print_grid(newGrid);
-	//}
-
-	while (true) {
+	let last_json = "";
+	do {
 		steps++;
+		last_json = JSON.stringify(grid);
 
-		let json = JSON.stringify(inputs);
-		if (json === last) {
-			let seats = 0;
-			for (let r = 0; r < inputs.length; r++) {
-				for (let c = 0; c < inputs[r].length; c++) {
-					if (inputs[r][c] == '#') {
-						seats++;
-					}
-				}
-			}
-			console.log(`Stabilized after ${steps} steps.  Seats = ${seats}`);
-
-			break;
-		} else {
-			last = json;
-		}
-
-		let newGrid = JSON.parse(json);
-
-		let x = count_adjacent2(inputs, 1, 1);
-
-		for (let r = 0; r < inputs.length; r++) {
-			for (let c = 0; c < inputs[r].length; c++) {
-				const ch = inputs[r][c];
-
-				if (ch == 'L' && count_adjacent2(inputs, r, c) == 0) {
+		let newGrid = grid.map(row => row.slice()); // clone grid
+		for (let r = 0; r < grid.length; r++) {
+			for (let c = 0; c < grid[r].length; c++) {
+				const ch = grid[r][c];
+				if (ch == 'L' && count_adjacent(grid, r, c) == 0) {
 					newGrid[r][c] = "#";
-				} else if (ch == '#' && count_adjacent2(inputs, r, c) >= 5) {
+				} else if (ch == '#' && count_adjacent(grid, r, c) >= 4) {
 					newGrid[r][c] = 'L';
 				}
 			}
 		}
-		print_grid(newGrid);
-		inputs = newGrid;
-	}
+		grid = newGrid;
+	} while (JSON.stringify(grid) != last_json);
 
-	inputs
+	console.log(`Stabilized after ${steps} steps.  Seats = ${count_seats(grid)}`);
+}
+
+function part2(grid: string[][]) {
+	let steps = 0;
+	let last_json = "";
+
+	do {
+		steps++;
+		last_json = JSON.stringify(grid);
+
+		let newGrid = grid.map(row => row.slice()); // clone grid
+		for (let r = 0; r < grid.length; r++) {
+			for (let c = 0; c < grid[r].length; c++) {
+				const ch = grid[r][c];
+				if (ch == 'L' && count_adjacent2(grid, r, c) == 0) {
+					newGrid[r][c] = "#";
+				} else if (ch == '#' && count_adjacent2(grid, r, c) >= 5) {
+					newGrid[r][c] = 'L';
+				}
+			}
+		}
+		grid = newGrid;
+	} while (JSON.stringify(grid) != last_json);
+
+	console.log(`Stabilized after ${steps} steps.  Seats = ${count_seats(grid)}`);
+}
+
+export async function run() {
+	let inputs = (await getPuzzleInput()).map(i => i.split(''));
+	part1(inputs.map(row => row.slice()));
+	part2(inputs.map(row => row.slice()));
 }
 
 run();
