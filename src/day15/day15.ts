07b4f48aa06e5ed8e39445f0cf863ea47fc5f58c
Galen Elias <gelias@gmail.com>
2020-12-15 09:05:46 -0800

    Day 15 - Minor cleanup tweaks

--- src/day15/day15.ts
+++ src/day15/day15.ts
@@ -2,24 +2,18 @@ import { getPuzzleInput } from '../util/input.js'
 
 async function run() {
 	const inputs = await getPuzzleInput();
-
 	const nums = inputs[0].split(',').map((v) => +v);
 
-	let turn = 1;
 	let spoke = new Map<number, number>();
 
 	for (let i = 0; i < nums.length-1; i++) {
-		spoke.set(nums[i], turn);
-		turn++;
+		spoke.set(nums[i], i + 1);
 	}
 
 	let next = nums.slice(-1)[0];
-	while (true) {
+	for (let turn = nums.length; turn != 30000000; ++turn) {
 		if (turn === 2020 ) {
 			console.log("Part 1", next);
-		} else if (turn === 30000000 ) {
-			console.log("Part 2", next);
-			break;
 		}
 
 		let last_spoke = spoke.get(next);
@@ -29,9 +23,8 @@ async function run() {
 		} else {
 			next = turn - last_spoke;
 		}
-
-		turn++;
 	}
+	console.log("Part 2", next);
 }
 
 run();
--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -3,21 +3,16 @@ use std::collections::HashMap;
 
 pub fn solve(inputs: Vec<String>) {
 	let nums = inputs[0].split(',').map(|v| v.parse::<u32>().unwrap()).collect_vec();
-
 	let mut spoke: HashMap<u32, u32> = HashMap::new();
 
 	for i in 0..nums.len()-1 {
 		spoke.insert(nums[i], (i + 1) as u32);
 	}
 
-	let mut turn = nums.len() as u32;
 	let mut next = nums[nums.len()-1];
-	loop {
+	for turn in (nums.len() as u32)..30000000 {
 		if turn == 2020  {
 			println!("Part 1: {}", next);
-		} else if turn == 30000000 {
-			println!("Part 2: {}", next);
-			break;
 		}
 
 		if let Some(last_spoke) = spoke.insert(next, turn) {
@@ -25,7 +20,6 @@ pub fn solve(inputs: Vec<String>) {
 		} else {
 			next = 0;
 		}
-
-		turn += 1;
 	}
+	println!("Part 2: {}", next);
 }
