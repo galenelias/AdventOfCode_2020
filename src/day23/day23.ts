86ec4f9528b16186bc7a4133957020ba75f64331
Galen Elias <gelias@gmail.com>
2020-12-23 21:53:57 -0800

    Day 23 - TypeScript

---
+++ src/day23/day23.ts
@@ -0,0 +1,61 @@
+import { getPuzzleInput } from '../util/input.js'
+
+function traverseLL(ll: number[]): number[] {
+	let result: number[] = [1];
+	for (let i = 1; ll[i] != 1; i = ll[i]) {
+		result.push(ll[i]);
+	}
+	return result;
+}
+
+function run_game(cups: number[], is_part1: boolean, cups_fill_amt: number, iterations: number) {
+	let ll: number[] = new Array(cups.length+1);
+	for (let i = 0; i < cups.length - 1; i++) {
+		ll[cups[i]] = cups[i+1];
+	}
+
+	if (cups_fill_amt === 0) {
+		ll[cups[cups.length - 1]] = cups[0];
+	} else {
+		for (let i = cups.length + 1; i < cups_fill_amt; ++i) {
+			ll[i] = i + 1;
+		}
+
+		ll[cups[cups.length - 1]] = cups.length + 1;
+		ll[ll.length] = cups[0];
+	}
+
+	let current = cups[0];
+	for (let move = 0; move < iterations; move++) {
+		let three_cups = [ll[current], ll[ll[current]], ll[ll[ll[current]]]];
+		ll[current] = ll[three_cups[2]]; // Re-link current to the end of our three cup chain
+
+		let dest_cup = current -1;
+		if (dest_cup === 0) {
+			dest_cup = ll.length - 1;
+		}
+		while (three_cups.find(v => v === dest_cup)) {
+			dest_cup = (dest_cup === 1) ? ll.length - 1 : dest_cup - 1;
+		}
+
+		ll[three_cups[2]] = ll[dest_cup];
+		ll[dest_cup] = three_cups[0];
+		current = ll[current];
+	}
+
+	if (is_part1) {
+		let part1 = traverseLL(ll).slice(1).reduce((acc, v) => acc + v, "");
+		console.log("Part 1:", part1);
+	} else {
+		console.log(`Part 2: ${ll[1] * ll[ll[1]]} (${ll[1]} * ${ll[ll[1]]})`);
+	}
+}
+
+async function run() {
+	const inputs = await getPuzzleInput();
+	let cups = inputs[0].split("").map(v => +v);
+	run_game(cups, true, 0, 100);
+	run_game(cups, false, 1000000, 10000000);
+}
+
+run();
